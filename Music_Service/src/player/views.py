8a5dac899e3403f3824d2f3f1abd8f7f8aa5e503
Hailemichael Atrsaw <hailat.alx@gmail.com>
2022-07-22 12:40:26 +0300

    Edited admin and views

--- Music_Service/src/player/views.py
+++ Music_Service/src/player/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
 # Create your views here.
+
+from .models import Artist, Album, Genre, Track, Lyrics
+from .serializers import ArtistSerializer, AlbumSerializer, GenreSerializer, LyricsSerializer, TrackSerializer, \
+    GenreSerializer, TrackSerializer, LyricsSerializer
+
+class ArtistViewSet(viewsets.ModelViewSet):
+    
+    queryset = Artist.objects.all()
+    serializer_class = ArtistSerializer
+
+class AlbumViewSet(viewsets.ModelViewSet):
+    
+    queryset = Album.objects.all()
+    serializer_class = AlbumSerializer
+
+class GenreViewSet(viewsets.ModelViewSet):
+    
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+class TrackViewSet(viewsets.ModelViewSet):
+    
+    queryset = Track.objects.all()
+    serializer_class = TrackSerializer
+
+class LyricsViewSet(viewsets.ModelViewSet):
+    
+    queryset = Lyrics.objects.all()
+    serializer_class = LyricsSerializer
