370795093c5b23f81236a5e092a8a3634e72f7ba
Hailemichael Atrsaw <hailat.alx@gmail.com>
2022-07-22 12:18:43 +0300

    Edit core/urls.py

--- Music_Service/src/core/urls.py
+++ Music_Service/src/core/urls.py
@@ -15,8 +15,32 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+from .router import router
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Controller Service API",
+      default_version='v1',
+      description="Controller Service API description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="admin@kinideas.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
-]
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/', include(router.urls)),
+    path('OpenAPI_Config_Music_Service.yaml', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
