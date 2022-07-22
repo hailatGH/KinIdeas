33e52efd304a860ea0f6e6ef186bfd9f9ce5d75a
Hailemichael Atrsaw <hailat.alx@gmail.com>
2022-07-22 12:49:38 +0300

    Code works

--- Music_Service/src/player/admin.py
+++ Music_Service/src/player/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
-from .models import Artist, Album, Track, Genre
+from .models import Artist, Album, Track, Genre, Lyrics
 
 # Register your models here.
 
 admin.site.register(Artist)
 admin.site.register(Album)
-admin.site.register(Track)
 admin.site.register(Genre)
+admin.site.register(Track)
+admin.site.register(Lyrics)
