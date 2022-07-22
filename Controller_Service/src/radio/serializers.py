8dbf855ff846c162ca5aeed0e76a838e8f466660
Hailemichael Atrsaw <hailat.alx@gmail.com>
2022-07-22 11:05:48 +0300

    controller service removed

--- Controller_Service/src/radio/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-
-from .models import Radio
-
-class RadioSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Radio
-        fields = '__all__'
