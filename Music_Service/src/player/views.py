374b403c82505124af73c0ed8adc80d8d48594db
Hailemichael Atrsaw <hailat.alx@gmail.com>
2022-07-22 17:31:56 +0300

    Pagination done

--- Music_Service/src/player/views.py
+++ Music_Service/src/player/views.py
@@ -1,32 +1,45 @@
 from rest_framework import viewsets
-
-# Create your views here.
+from rest_framework.pagination import PageNumberPagination
 
 from .models import Artist, Album, Genre, Track, Lyrics
 from .serializers import ArtistSerializer, AlbumSerializer, GenreSerializer, LyricsSerializer, TrackSerializer, \
     GenreSerializer, TrackSerializer, LyricsSerializer
 
+# Pagination
+
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 1000
+
+# Create your views here.
+
 class ArtistViewSet(viewsets.ModelViewSet):
     
     queryset = Artist.objects.all()
     serializer_class = ArtistSerializer
+    pagination_class = StandardResultsSetPagination
 
 class AlbumViewSet(viewsets.ModelViewSet):
     
     queryset = Album.objects.all()
     serializer_class = AlbumSerializer
+    pagination_class = StandardResultsSetPagination
 
 class GenreViewSet(viewsets.ModelViewSet):
     
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
+    pagination_class = StandardResultsSetPagination
 
 class TrackViewSet(viewsets.ModelViewSet):
     
     queryset = Track.objects.all()
     serializer_class = TrackSerializer
+    pagination_class = StandardResultsSetPagination
 
 class LyricsViewSet(viewsets.ModelViewSet):
     
     queryset = Lyrics.objects.all()
     serializer_class = LyricsSerializer
+    pagination_class = StandardResultsSetPagination
