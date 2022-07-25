34bf4d95023d8224d0199cd5f6b08c8194af4537
Hailemichael Atrsaw <hailat.alx@gmail.com>
2022-07-25 11:49:34 +0300

    added dummy data

--- Music_Service/src/player/views.py
+++ Music_Service/src/player/views.py
@@ -8,7 +8,7 @@ from .serializers import ArtistSerializer, AlbumSerializer, GenreSerializer, Tra
 # Pagination
 
 class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 2
+    page_size = 1000
     page_size_query_param = 'page_size'
     # max_page_size = 1000
 
