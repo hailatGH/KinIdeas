6396d1dab3eaf346f7e3242d2c31263a53afca86
Hailemichael Atrsaw <hailat.alx@gmail.com>
2022-07-26 15:07:21 +0300

    dictionary created

--- Music_Service/src/player/players.py
+++ Music_Service/src/player/players.py
@@ -6,16 +6,28 @@ from .models import Artist, Album, Genre, PlayListTracks, Track, Lyrics, PlayLis
 
 @api_view()
 def hello_world(request):
-    track_id = request.GET['track']
-    track_data = Track.objects.filter(id=track_id).values('id', 'track_name', 'track_file', 'artist_id', 'album_id', \
+    track_data = {}
+    
+    track_obj = Track.objects.filter(id=request.GET['track']).values('id', 'track_name', 'track_file', 'artist_id', 'album_id', \
         "genre_id")
+    track_id = track_obj[0]['id']
+    track_name = track_obj[0]['track_name']
+    track_file = track_obj[0]['track_file']
+    artist_id = track_obj[0]['artist_id']
+    artist_name = Artist.objects.filter(id=track_obj[0]['artist_id']).values('artist_name')[0]['artist_name']
+    album_id = track_obj[0]['album_id']
+    album_title = Album.objects.filter(id=track_obj[0]['album_id']).values('album_title')[0]['album_title']
+    album_cover = Album.objects.filter(id=track_obj[0]['album_id']).values('album_cover')[0]['album_cover']
+    genre_id = track_obj[0]['genre_id']
+    genre_title = Genre.objects.filter(id=track_obj[0]['genre_id']).values('genre_title')[0]['genre_title']
+    lyrics_id = Lyrics.objects.filter(track_id=track_id).values('id')[0]['id']
+    lyrics_detail = Lyrics.objects.filter(track_id=track_id).values('lyrics_detail')[0]['lyrics_detail']
 
-    print()
-    artist_name = Artist.objects.filter(id=track_data[0]['artist_id']).values('artist_name')
-    # artist_id = Track.objects.filter(id=track_id).values('artist_id')[0]['artist_id']
-    # album_id = Track.objects.filter(id=track_id).values('album_id')[0]['album_id']
-    # genre_id = Track.objects.filter(id=track_id).values('album_id')[0]['album_id']
-    return Response({"Track_Data": artist_name})
+    for variable in ["track_id", "track_name", "track_file", "artist_id", "artist_name", "album_id",\
+        "album_title", "album_cover", "genre_id", "genre_title", "lyrics_id", "lyrics_detail"]:
+        track_data[variable] = eval(variable)
+
+    return Response(track_data)
 
 # class SingleMusicPlayer(views):
 #     def get_query(self):
