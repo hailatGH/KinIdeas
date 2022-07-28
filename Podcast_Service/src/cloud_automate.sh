ee57298d56ec8892604d097c3c9eaa2d312c7ddf
Hailemichael Atrsaw <hailat.alx@gmail.com>
2022-07-28 11:09:35 +0300

    cloud automate script completed

--- Podcast_Service/src/cloud_automate.sh
+++ Podcast_Service/src/cloud_automate.sh
@@ -13,10 +13,10 @@ gcloud services enable \
 PROJECT_ID=$(gcloud config get-value core/project)
 REGION=europe-west1
 
-gcloud iam service-accounts create music-service-account
+gcloud iam service-accounts create podcast-service-account
 
 SERVICE_ACCOUNT=$(gcloud iam service-accounts list \
-    --filter music-service-account --format "value(email)")
+    --filter podcast-service-account --format "value(email)")
 
 # gcloud sql instances create kin-project-postgresql-v2 \
 #   --project $PROJECT_ID \
@@ -25,10 +25,10 @@ SERVICE_ACCOUNT=$(gcloud iam service-accounts list \
 #   --memory=7680MB \
 #   --region $REGION
 
-gcloud sql databases create music-database --instance kin-project-postgresql-v2
+gcloud sql databases create podcast-database --instance kin-project-postgresql-v2
 
-music_database_admin_password="$(cat /dev/urandom | LC_ALL=C tr -dc 'a-zA-Z0-9' | fold -w 30 | head -n 1)"
-gcloud sql users create music_database_admin --instance kin-project-postgresql-v2 --password $music_database_admin_password
+podcast_database_admin_password="$(cat /dev/urandom | LC_ALL=C tr -dc 'a-zA-Z0-9' | fold -w 30 | head -n 1)"
+gcloud sql users create podcast_database_admin --instance kin-project-postgresql-v2 --password $podcast_database_admin_password
 
 gcloud projects add-iam-policy-binding $PROJECT_ID \
     --member serviceAccount:${SERVICE_ACCOUNT} \
@@ -41,20 +41,20 @@ gcloud projects add-iam-policy-binding $PROJECT_ID \
 GS_BUCKET_NAME=${PROJECT_ID}-storage
 gsutil mb -l ${REGION} gs://${GS_BUCKET_NAME}
 
-echo DATABASE_URL=\"postgres://music_database_admin:${music_database_admin_password}@//cloudsql/${PROJECT_ID}:${REGION}:kin-project-postgresql-v2/music-database\" > .env
+echo DATABASE_URL=\"postgres://podcast_database_admin:${podcast_database_admin_password}@//cloudsql/${PROJECT_ID}:${REGION}:kin-project-postgresql-v2/podcast-database\" > .env
 echo GS_BUCKET_NAME=\"${GS_BUCKET_NAME}\" >> .env
 echo SECRET_KEY=\"$(cat /dev/urandom | LC_ALL=C tr -dc 'a-zA-Z0-9' | fold -w 50 | head -n 1)\" >> .env
 echo DEBUG=\"True\" >> .env
 
-gcloud secrets create music_service_settings --data-file .env
-gcloud secrets add-iam-policy-binding music_service_settings \
+gcloud secrets create podcast_service_settings --data-file .env
+gcloud secrets add-iam-policy-binding podcast_service_settings \
   --member serviceAccount:${SERVICE_ACCOUNT} --role roles/secretmanager.secretAccessor
 rm .env
 
 export PROJECTNUM=$(gcloud projects describe ${PROJECT_ID} --format 'value(projectNumber)')
 export CLOUDBUILD=${PROJECTNUM}@cloudbuild.gserviceaccount.com
 
-gcloud secrets add-iam-policy-binding music_service_settings \
+gcloud secrets add-iam-policy-binding podcast_service_settings \
   --member serviceAccount:${CLOUDBUILD} --role roles/secretmanager.secretAccessor
 
 gcloud projects add-iam-policy-binding ${PROJECT_ID} \
@@ -63,14 +63,14 @@ gcloud projects add-iam-policy-binding ${PROJECT_ID} \
 gcloud projects add-iam-policy-binding ${PROJECT_ID} \
     --member serviceAccount:${CLOUDBUILD} --role roles/storage.admin
 
-music_admin_password="$(cat /dev/urandom | LC_ALL=C tr -dc 'a-zA-Z0-9' | fold -w 30 | head -n 1)"
+podcast_admin_password="$(cat /dev/urandom | LC_ALL=C tr -dc 'a-zA-Z0-9' | fold -w 30 | head -n 1)"
 
-echo -n "${music_admin_password}" | gcloud secrets create music_admin_password --data-file=-
+echo -n "${podcast_admin_password}" | gcloud secrets create podcast_admin_password --data-file=-
 
-gcloud secrets add-iam-policy-binding music_admin_password \
+gcloud secrets add-iam-policy-binding podcast_admin_password \
   --member serviceAccount:${CLOUDBUILD} --role roles/secretmanager.secretAccessor
 
-gcloud builds submit --region=$REGION --pack image=gcr.io/${PROJECT_ID}/music_service_image
+gcloud builds submit --region=$REGION --pack image=gcr.io/${PROJECT_ID}/podcast_service_image
 
 gcloud builds submit --config migrate.yaml --substitutions _REGION=$REGION
 
@@ -80,34 +80,34 @@ gcloud projects add-iam-policy-binding ${PROJECT_ID} \
 gcloud run deploy music-service \
   --platform managed \
   --region $REGION \
-  --image gcr.io/${PROJECT_ID}/music_service_image \
+  --image gcr.io/${PROJECT_ID}/podcast_service_image \
   --set-cloudsql-instances ${PROJECT_ID}:${REGION}:kin-project-postgresql-v2 \
-  --set-secrets MUSIC_SERVICE_SETTINGS=music_service_settings:latest \
+  --set-secrets PODCAST_SERVICE_SETTINGS=podcast_service_settings:latest \
   --service-account $SERVICE_ACCOUNT \
   --allow-unauthenticated
 
-MUSIC_SERVICE_URL=$(gcloud run services describe music-service \
+PODCAST_SERVICE_URL=$(gcloud run services describe podcast-service \
   --platform managed \
   --region $REGION  \
   --format "value(status.url)")
 
-gcloud secrets versions access latest --secret music_service_settings > temp_settings
-echo MUSIC_SERVICE_URL=${MUSIC_SERVICE_URL} >> temp_settings
-gcloud secrets versions add music_service_settings --data-file temp_settings
+gcloud secrets versions access latest --secret podcast_service_settings > temp_settings
+echo PODCAST_SERVICE_URL=${PODCAST_SERVICE_URL} >> temp_settings
+gcloud secrets versions add podcast_service_settings --data-file temp_settings
 rm temp_settings
 
-gcloud run services update music-service \
+gcloud run services update podcast-service \
   --platform managed \
   --region $REGION \
-  --image gcr.io/${PROJECT_ID}/music_service_image
+  --image gcr.io/${PROJECT_ID}/podcast_service_image
 
-gcloud secrets versions access latest --secret music_admin_password && echo ""
+gcloud secrets versions access latest --secret podcast_admin_password && echo ""
 
-gcloud builds submit --pack image=gcr.io/${PROJECT_ID}/music_service_image
+# gcloud builds submit --pack image=gcr.io/${PROJECT_ID}/podcast_service_image
 
-gcloud builds submit --config migrate.yaml --substitutions _REGION=$REGION
+# gcloud builds submit --config migrate.yaml --substitutions _REGION=$REGION
 
-gcloud run services update music-service \
-  --platform managed \
-  --region $REGION \
-  --image gcr.io/${PROJECT_ID}/music_service_image
+# gcloud run services update podcast-service \
+#   --platform managed \
+#   --region $REGION \
+#   --image gcr.io/${PROJECT_ID}/podcast_service_image
