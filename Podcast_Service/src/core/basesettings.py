3e3b9d8b5c96be53d42643656158b26ed9014091
Hailemichael Atrsaw <hailat.alx@gmail.com>
2022-07-28 16:14:49 +0300

    music cors policy fix

--- Podcast_Service/src/core/basesettings.py
+++ Podcast_Service/src/core/basesettings.py
@@ -144,9 +144,11 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
 
-CORS_ALLOWED_ORIGINS = []
+# CORS_ALLOWED_ORIGINS = []
 
-CSRF_TRUSTED_ORIGINS = []
+# CSRF_TRUSTED_ORIGINS = []
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_METHODS = [
     "DELETE",
