ceb84bdc3dffdf7a202ddf2d5631ccd9951de589
Hailemichael Atrsaw <hailat.alx@gmail.com>
2022-07-29 16:28:00 +0300

    done

--- Radio_Service/src/cleanup_automate.sh
+++ Radio_Service/src/cleanup_automate.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-gcloud iam service-accounts delete music-service-account@kin-project-352614.iam.gserviceaccount.com
-gcloud sql databases delete music-database --instance kin-project-postgresql-v2
-gcloud sql users delete music_database_admin --instance kin-project-postgresql-v2
-gcloud secrets delete music_service_settings
-gcloud secrets delete music_admin_password
-gcloud run services delete music-service
+
+gcloud config set account hailemichael.atrsaw@kinideas.com
+gcloud config set project kin-project-352614
+
+gcloud iam service-accounts delete radio-service-account@kin-project-352614.iam.gserviceaccount.com
+gcloud sql databases delete radio-database --instance kin-project-postgresql-v2
+gcloud sql users delete radio_database_admin --instance kin-project-postgresql-v2
+gcloud secrets delete radio_service_settings
+gcloud secrets delete radio_admin_password
+gcloud run services delete radio-service
